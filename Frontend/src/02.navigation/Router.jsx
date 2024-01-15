99ff5eac0c8f95498b1a8058f760150199ff9af0
Tommy <tommy.angibaud@gmail.com>
2024-01-15 17:24:45 +0100

    🎨✨ added style to the login page

--- Frontend/src/02.navigation/Router.jsx
+++ Frontend/src/02.navigation/Router.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "../03.globals/01.Pages/HomePage";
-
+import Login from "../03.globals/01.Pages/Login"
 
 function Router() {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            
+            <Route path="/login" element={<Login/>}/>
             
             
             // redirection automatique si la page n'existe pas
