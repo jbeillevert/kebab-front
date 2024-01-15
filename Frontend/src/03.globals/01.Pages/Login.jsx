4783087e7369c8f92c860764b98a160d3b79b689
Tommy <tommy.angibaud@gmail.com>
2024-01-15 18:57:10 +0100

    ➕ added functionality to test the connection

--- Frontend/src/03.globals/01.Pages/Login.jsx
+++ Frontend/src/03.globals/01.Pages/Login.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from "axios";
+// import { NavLink } from "react-router-dom";
+
+
+
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = () => {
+    axios
+        .post("http://localhost:3000/login", {email: email, password: password})
+        .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.warn("retour err", err);  
+    });
+}
+
+
     return (
         <div className='h-full w-full flex items-center justify-center'>
-                <form action="submit" className='bg-white border-solid border-2 h-1/2 w-1/2 border-orange-600 flex flex-col justify-center p-16 rounded-lg gap-4'>
+                <form action="" className='bg-white border-solid border-2 h-1/2 w-1/2 border-orange-600 flex flex-col justify-center p-16 rounded-lg gap-4' onSubmit={handleSubmit} >
                     <h1 className='font-black text-2xl'>Content de vous revoir</h1>
                     <p className='text-2xl'>Connectez-vous et créer vos recettes</p>
                     <div className='flex flex-col'>
-                        <label htmlFor="">Votre mail</label>
-                        <input className="border-2 border-solid border-orange-600 rounded-sm h-[32px] pl-5" placeholder="Email@NomDeDomaine.com" type="text" />
+                        <label htmlFor="email">Votre mail</label>
+                        <input id='email' className={`border-2 border-solid border-orange-600 rounded-sm h-[32px] pl-5 `} onChange={(event) => setEmail(event.target.value)} placeholder="Email@NomDeDomaine.com" type="text" />
                     </div>
                     <div className='flex flex-col gap'>
-                        <label htmlFor="">Votre password</label>
-                        <input className="border-2 border-solid border-orange-600 rounded-sm h-[32px] pl-5" placeholder="Password" type="text" />
+                        <label htmlFor="password">Votre password</label>
+                        <input id='password' type="password" className={`border-2 border-solid border-orange-600 rounded-sm h-[32px] pl-5`} onChange={(event) => setPassword(event.target.value)} placeholder="Password"/>
                     </div>
                     <button className='bg-orange-600 rounded-xl m-5 py-2 text-white' type='submit'>Se Connecter</button>
                     <div className=' w-full flex flex-row justify-center gap-5 items-center'>
@@ -21,8 +42,11 @@ const Login = () => {
                         <div className='h-[1px] w-[11rem] bg-orange-600'></div>
                     </div>
                     
-                    <p>Vous n'avez pas encore de compte ? <button className='text-orange-600 font-bold'>Créer un compte</button>
-                    </p>
+                    <span>Vous n'avez pas encore de compte ? 
+                        {/* <NavLink to="/Register"> */}
+                            <p className='text-orange-600 font-bold'>Créer un compte</p>
+                        {/* </NavLink> */}
+                    </span>
                 </form>
         </div>
     );
