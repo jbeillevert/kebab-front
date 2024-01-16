62b7e8ec1e1cd7b772f69f224ff3e5d6bb535762
AntoineBonamy <bonamy.antoine@hotmail.fr>
2024-01-16 17:20:26 +0100

    En tant qu’utilisateur je veux voir le detail d’une recette. Ajout de la page détails de la recette.

--- Frontend/src/03.globals/01.Pages/HomePage.jsx
+++ Frontend/src/03.globals/01.Pages/HomePage.jsx
@@ -1,18 +1,19 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 // import Navbar from '../03.Components/Navbar';
 import axios from 'axios'
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 const HomePage = () => {
-    const [data,setData] = useState([])
+    const [data, setData] = useState([])
 
-    useEffect (()=>{
+    useEffect(() => {
         axios
-        .get(`http://localhost:3000/recipes`)
-        .then((res) => {setData(res.data.data)
-        })
-    },[])
+            .get(`http://localhost:3000/recipes`)
+            .then((res) => {
+                setData(res.data.data)
+            })
+    }, [])
 
     console.log(data);
     return (
@@ -27,17 +28,19 @@ const HomePage = () => {
                 </div>
             </div>
             <div className='flex flex-wrap p-8 gap-8 m-auto'>
-                {data.map((e, index)=> {
-                    return(
-                    <div key={index} className='bg-white min-w-64 w-80 h-52 rounded-lg shadow-sm hover:shadow-md duration-150  cursor-pointer max-w-52 flex flex-col'>
-                    <img className='bg-neutral-400 w-full h-3/4 rounded-t-lg'></img>
-                        <div className='p-2 flex flex-col gap-2'>
-                            <h3 className=''>{e.name}</h3>
-                        <button className='bg-orange-400 w-fit py-1 px-4 rounded-md text-white font-medium hover:bg-orange-500 duration-150' value={e.id}>Voir</button>
+                {data.map((e, index) => {
+                    return (
+                        <div key={index} className='bg-white min-w-64 w-80 h-52 rounded-lg shadow-sm hover:shadow-md duration-150  cursor-pointer max-w-52 flex flex-col'>
+                            <img className='bg-neutral-400 w-full h-3/4 rounded-t-lg'></img>
+                            <div className='p-2 flex flex-col gap-2'>
+                                <h3 className=''>{e.name}</h3>
+                                <NavLink to={`/recipes/${e.id}`}>
+                                    <button className='bg-orange-400 w-fit py-1 px-4 rounded-md text-white font-medium hover:bg-orange-500 duration-150' value={e.id}>Voir</button>
+                                </NavLink>
+                            </div>
                         </div>
-                    </div>
                     )
-                })}    
+                })}
             </div>
         </div>
     );
